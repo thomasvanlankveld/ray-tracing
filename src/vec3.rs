c3ddd32eb0ded0cc9e287f13874bcee121699b88
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2021-01-15 13:35:27 +0100

    test(Vec3): change f64 in float literals to .0

--- src/vec3.rs
+++ src/vec3.rs
@@ -111,24 +111,24 @@ impl DivAssign for Vec3 {
 fn test_instantiation() {
     // When I instantiate a Vec3
     let vec3 = Vec3 {
-        x: 24f64,
-        y: 35f64,
-        z: 46f64,
+        x: 24.0,
+        y: 35.0,
+        z: 46.0,
     };
 
     // Then I can read the values I instantiated it with
-    assert_eq!(vec3.x, 24f64);
-    assert_eq!(vec3.y, 35f64);
-    assert_eq!(vec3.z, 46f64);
+    assert_eq!(vec3.x, 24.0);
+    assert_eq!(vec3.y, 35.0);
+    assert_eq!(vec3.z, 46.0);
 }
 
 #[test]
 fn test_display() {
     // Given a Vec3
     let vec3 = Vec3 {
-        x: 24f64,
-        y: 35f64,
-        z: 46f64,
+        x: 24.0,
+        y: 35.0,
+        z: 46.0,
     };
 
     // When I format it as a string
@@ -142,123 +142,123 @@ fn test_display() {
 fn test_negation() {
     // Given a Vec3
     let vec3 = Vec3 {
-        x: 24f64,
-        y: 35f64,
-        z: 46f64,
+        x: 24.0,
+        y: 35.0,
+        z: 46.0,
     };
 
     // When I negate it
     let negated = -vec3;
 
     // Then its values are negated
-    assert_eq!(negated.x, -24f64);
-    assert_eq!(negated.y, -35f64);
-    assert_eq!(negated.z, -46f64);
+    assert_eq!(negated.x, -24.0);
+    assert_eq!(negated.y, -35.0);
+    assert_eq!(negated.z, -46.0);
 }
 
 #[test]
 fn test_indexing() {
     // Given a Vec3
     let vec3 = Vec3 {
-        x: 24f64,
-        y: 35f64,
-        z: 46f64,
+        x: 24.0,
+        y: 35.0,
+        z: 46.0,
     };
 
     // When I access its values by indices
     // Then I can read its values
-    assert_eq!(vec3[0], 24f64);
-    assert_eq!(vec3[1], 35f64);
-    assert_eq!(vec3[2], 46f64);
+    assert_eq!(vec3[0], 24.0);
+    assert_eq!(vec3[1], 35.0);
+    assert_eq!(vec3[2], 46.0);
 }
 
 #[test]
 fn test_mutable_indexing() {
     // Given a Vec3
     let mut vec3 = Vec3 {
-        x: 24f64,
-        y: 35f64,
-        z: 46f64,
+        x: 24.0,
+        y: 35.0,
+        z: 46.0,
     };
 
     // When I write to its values by indices
-    vec3[0] = 0f64;
-    vec3[1] = 1f64;
-    vec3[2] = 2f64;
+    vec3[0] = 0.0;
+    vec3[1] = 1.0;
+    vec3[2] = 2.0;
 
     // Then I can read the values I wrote
-    assert_eq!(vec3.x, 0f64);
-    assert_eq!(vec3.y, 1f64);
-    assert_eq!(vec3.z, 2f64);
+    assert_eq!(vec3.x, 0.0);
+    assert_eq!(vec3.y, 1.0);
+    assert_eq!(vec3.z, 2.0);
 }
 
 #[test]
 fn test_add_assign() {
     // Given two Vec3s
     let mut vec3_first = Vec3 {
-        x: 0f64,
-        y: 1f64,
-        z: 2f64,
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
     };
     let vec3_second = Vec3 {
-        x: 3f64,
-        y: 4f64,
-        z: 5f64,
+        x: 3.0,
+        y: 4.0,
+        z: 5.0,
     };
 
     // When I add assign the second to the first
     vec3_first += vec3_second;
 
     // Then the first contains the sum of the addition
-    assert_eq!(vec3_first.x, 3f64);
-    assert_eq!(vec3_first.y, 5f64);
-    assert_eq!(vec3_first.z, 7f64);
+    assert_eq!(vec3_first.x, 3.0);
+    assert_eq!(vec3_first.y, 5.0);
+    assert_eq!(vec3_first.z, 7.0);
 }
 
 #[test]
 fn test_mul_assign() {
     // Given two Vec3s
     let mut vec3_first = Vec3 {
-        x: 0f64,
-        y: 1f64,
-        z: 2f64,
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
     };
     let vec3_second = Vec3 {
-        x: 3f64,
-        y: 4f64,
-        z: 5f64,
+        x: 3.0,
+        y: 4.0,
+        z: 5.0,
     };
 
     // When I multiply assign the first by the second
     vec3_first *= vec3_second;
 
     // Then the first contains the product of the multiplication
-    assert_eq!(vec3_first.x, 0f64);
-    assert_eq!(vec3_first.y, 4f64);
-    assert_eq!(vec3_first.z, 10f64);
+    assert_eq!(vec3_first.x, 0.0);
+    assert_eq!(vec3_first.y, 4.0);
+    assert_eq!(vec3_first.z, 10.0);
 }
 
 #[test]
 fn test_div_assign() {
     // Given two Vec3s
     let mut vec3_first = Vec3 {
-        x: 0f64,
-        y: 1f64,
-        z: 2f64,
+        x: 0.0,
+        y: 1.0,
+        z: 2.0,
     };
     let vec3_second = Vec3 {
-        x: 3f64,
-        y: 4f64,
-        z: 5f64,
+        x: 3.0,
+        y: 4.0,
+        z: 5.0,
     };
 
     // When I divide assign the first by the second
     vec3_first /= vec3_second;
 
     // Then the first contains the result of the division
-    assert_eq!(vec3_first.x, 0f64);
-    assert_eq!(vec3_first.y, 0.25f64);
-    assert_eq!(vec3_first.z, 0.4f64);
+    assert_eq!(vec3_first.x, 0.0);
+    assert_eq!(vec3_first.y, 0.25);
+    assert_eq!(vec3_first.z, 0.4);
 }
 
 #[test]

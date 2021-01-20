d91c88796955d4312a96754ee4e77bc46fea1a3a
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2021-01-20 16:29:09 +0100

    feat(7.19): implement antialiasing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::error::Error;
 
 // Project
+mod camera;
 mod color;
 mod hittable;
 mod hittable_list;
@@ -10,17 +11,20 @@ mod ppm;
 mod ray;
 mod sphere;
 mod vec3;
+use camera::Camera;
 use color::Color;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
 use point3::Point3;
 use ppm::write_ppm;
+use rand::random;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::Vec3;
 
 // 3rd party
 extern crate conv;
+extern crate rand;
 use conv::*;
 
 #[allow(dead_code)]
@@ -59,38 +63,34 @@ fn ray_color(ray: Ray, world: &dyn Hittable) -> Color {
 fn main() -> Result<(), Box<dyn Error>> {
     // Image
     let aspect_ratio: f64 = 16. / 9.;
-    let image_width: u16 = 1200;
+    let image_width: u16 = 400;
     let image_height: u16 = (f64::from(image_width) / aspect_ratio).floor() as u16;
+    let samples_per_pixel = 100;
 
     // World
     let mut world = HittableList::new();
     world.add(Box::new(Sphere::new(Point3::new(0., 0., -1.), 0.5)));
-    world.add(Box::new(Sphere::new(Point3::new(-1., 0., -2.), 0.5)));
-    world.add(Box::new(Sphere::new(Point3::new(-2., 0., -3.), 0.5)));
+    // world.add(Box::new(Sphere::new(Point3::new(-1., 0., -2.), 0.5)));
+    // world.add(Box::new(Sphere::new(Point3::new(-2., 0., -3.), 0.5)));
     world.add(Box::new(Sphere::new(Point3::new(0., -100.5, -1.), 100.)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-    let origin = Point3::new(0., 0., 0.);
-    let horizontal = Vec3::new(viewport_width, 0., 0.);
-    let vertical = Vec3::new(0., viewport_height, 0.);
-    let lower_left_corner =
-        origin - horizontal / 2. - vertical / 2. - Vec3::new(0., 0., focal_length);
+    let camera = Camera::new();
 
     // Create pixel data
     let mut image = vec![vec![Color::new(0., 0., 0.); image_width.into()]; image_height.into()];
     for (y, row) in image.iter_mut().rev().enumerate() {
         for (x, pixel) in row.iter_mut().enumerate() {
-            // Construct ray for pixel
-            let u = f64::value_from(x)? / f64::value_from(image_width - 1)?;
-            let v = f64::value_from(y)? / f64::value_from(image_height - 1)?;
-            let direction = lower_left_corner + u * horizontal + v * vertical - origin;
-            let ray = Ray::new(origin, direction);
-
-            // Get pixel for ray
-            *pixel = ray_color(ray, &world);
+            // Per pixel, compute a number of randomly sampled values and add them all to the pixel
+            for _ in 0..(samples_per_pixel - 1) {
+                let u = (f64::value_from(x)? + random::<f64>()) / f64::value_from(image_width - 1)?;
+                let v =
+                    (f64::value_from(y)? + random::<f64>()) / f64::value_from(image_height - 1)?;
+                let ray = camera.get_ray(u, v);
+                *pixel += ray_color(ray, &world);
+            }
+            // Get average value of all samples by dividing by number of samples
+            *pixel /= f64::from(samples_per_pixel);
         }
     }
 

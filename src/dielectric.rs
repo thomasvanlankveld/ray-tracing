d25a7f053bc59d9e63f429415b3eb660ac083f04
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2021-01-21 17:31:29 +0100

    feat(10.35): implement Schlick glass approximation

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -1,3 +1,5 @@
+use rand::random;
+
 use crate::{color::Color, hittable::HitRecord, material::Material, point3::Vec3, ray::Ray};
 
 pub struct Dielectric {
@@ -8,6 +10,13 @@ impl Dielectric {
     pub fn new(refraction_index: f64) -> Self {
         Self { refraction_index }
     }
+}
+
+fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
+    // Use Schlick's approximation for reflectance.
+    let mut r0 = (1. - refraction_index) / (1. + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1. - r0) * f64::powi(1. - cosine, 5);
 }
 
 impl Material for Dielectric {
@@ -30,11 +39,12 @@ impl Material for Dielectric {
         let sin_theta = f64::sqrt(1. - cos_theta * cos_theta);
 
         let can_refract = refraction_ratio * sin_theta <= 1.;
-        let direction = if can_refract {
-            unit_direction.refract(record.normal, refraction_ratio)
-        } else {
-            unit_direction.reflect(record.normal)
-        };
+        let direction =
+            if can_refract && reflectance(cos_theta, refraction_ratio) <= random::<f64>() {
+                unit_direction.refract(record.normal, refraction_ratio)
+            } else {
+                unit_direction.reflect(record.normal)
+            };
 
         *scattered = Ray::new(record.point, direction);
 
